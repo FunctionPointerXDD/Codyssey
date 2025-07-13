e457b0b0c6af6c5f2245990ff3cc235c74f14fae
정 찬수 <kapustin4613@c4r4s1.codyssey.kr>
2025-07-13 17:05:14 +0900

    udpate

--- problem_2/power_calculator.py
+++ problem_2/power_calculator.py
@@ -1,5 +1,5 @@
 
-if __name__ == "__main__":
+def main():
     try:
         num = float(input())
     except:
@@ -12,9 +12,18 @@ if __name__ == "__main__":
         exit(1)
     
     Result = 1
-    while expo > 0:
-        Result *= num
-        expo -= 1
+    if expo >= 0:
+        while expo > 0:
+            Result *= num
+            expo -= 1
+    else:
+        if num == 0 and expo < 0:
+            print("Error: Division by zero due to negative exponent to zero.")
+        while expo < 0:
+            Result /= num
+            expo += 1
     print(Result)
-    
 
+
+if __name__ == "__main__":
+    main()
