97cecb6d40eaff686346fa188994549d682ed13c
kapustin4613 <kapustin4613@DESKTOP-OL3BKBP>
2025-07-13 10:47:16 +0900

    fix app.py

--- problem_2/app.py
+++ problem_2/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world() -> str:
-	return "Hello, DevOpszzzzz!"
+	return "Hello, DevOps!"
 
 if __name__ == "__main__":
 	app.run("0.0.0.0", 80, True)
