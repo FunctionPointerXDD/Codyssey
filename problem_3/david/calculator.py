5df2d0b92400bade14ed253e9cbc919e939700da
정 찬수 <kapustin4613@c4r4s1.codyssey.kr>
2025-07-15 16:57:55 +0900

    fix

--- problem_3/david/calculator.py
+++ problem_3/david/calculator.py
@@ -9,34 +9,33 @@ def multiply(a, b) -> int:
     return a * b
 
 def divide(a, b) -> float:
-    if b == 0:
-        print("Error:Division by zero.")
-        return None
     return a / b
 
 if __name__ == "__main__":
     try:
-        a = int(input("input number1: "))
-        b = int(input("input number2: "))
-    except:
+        a = float(input("input number1: "))
+        b = float(input("input number2: "))
+        a = int(a)
+        b = int(b)
+    
+        op = input("input operator: ")
+        if op == '+':
+            print("Result: ", add(a, b))
+        elif op == '-':
+            print("Result: ", subtract(a, b))
+        elif op == '*':
+            print("Result: ", multiply(a, b))
+        elif op == '/':
+            result = divide(a, b)
+            if result is not None:
+                print("Result: ", divide(a, b))
+        else:
+            raise ValueError('')
+
+    except ValueError:
+        
         print("Invalid input number.")
         exit(1)
-    
-    op = input("input operator: ")
-    if op == '+':
-        print("Result: ", add(a, b))
-    elif op == '-':
-        print("Result: ", subtract(a, b))
-    elif op == '*':
-        print("Result: ", multiply(a, b))
-    elif op == '/':
-        result = divide(a, b)
-        if result is not None:
-            print("Result: ", divide(a, b))
-    else:
-        print("Invalid operator.")
-
-
 
 
         
